2d8e4062b884a150186f8632853a3f08276a1449
Ben <39120147+bennygdev@users.noreply.github.com>
2023-01-17 12:15:38 +0800

    refactored code + error identifiers
    Refactored the whole code by getting rid of nested conditionals with try catch + for loop which takes 3 modes. Error identifiers for mode, words, quantity and wantnumber.

--- namegenerator/index.js
+++ namegenerator/index.js
@@ -6,43 +6,33 @@ const words = process.argv[3];
 const quantity = process.argv[4];
 const wantNumber = process.argv[5];
 
-if(mode == undefined || words == undefined || quantity == undefined || wantNumber == undefined) {
-    console.log(colors.red('Please enter a valid input'));
-} else {
-    if(mode == 'dashed') {
-        for (let i = 0; i < quantity; i++) {
-            let name;
-            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-                name = generate({words: words, number: true}).dashed;
-            } else {
-                name = generate({words: words}).dashed;
-            }
-            console.log(colors.green(name));
-        }
-    }
-    else if (mode == 'spaced') {
-        for (let i = 0; i < quantity; i++) {
-            let name;
-            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-                name = generate({words: words, number: true}).spaced;
-            } else {
-                name = generate({words: words}).spaced;
-            }
-            console.log(colors.green(name));
-        }
-    }
-    else if (mode == 'raw') {
-        for (let i = 0; i < quantity; i++) {
-            let name;
-            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-                name = generate({words: words, number: true}).raw;
-            } else {
-                name = generate({words: words}).raw;
-            }
-            console.log(colors.green(name));
-        }
+try {
+    if(mode == undefined || words == undefined || quantity == undefined || wantNumber == undefined) {
+        throw new Error('Please enter a valid input');
     } 
-    else {
-        console.log(colors.red('Invalid input, please try again'));
+    
+    if(mode != 'dashed' && mode != 'spaced' && mode != 'raw') {
+        throw new Error('Please enter a valid mode');
     }
+
+    if(isNaN(words) || isNaN(quantity)) {
+        throw new Error('Please enter a valid number for words and quantity');
+    }
+
+    if(wantNumber != "yes" && wantNumber != "y" && wantNumber != "Y" && wantNumber != "YES" && wantNumber != "n" && wantNumber != "N" && wantNumber != "no" && wantNumber != "NO") {
+        throw new Error('Invalid input for wantNumber, please input yes or no OR y or n for the argument.');
+    }
+
+    for(let i = 0; i < quantity; i++) {
+        let name;
+        if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
+            name = generate({words: words, number: true})[mode];
+        } else if (wantNumber === "n" || wantNumber === "N" || wantNumber === "no" || wantNumber === "NO"  ) {
+            name = generate({words: words})[mode];
+        }
+        console.log(colors.green(name));
+    }
+
+} catch (err) {
+    console.log(colors.red(err.message));
 }
