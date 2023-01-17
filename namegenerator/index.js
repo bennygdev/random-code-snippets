791b6acb92e10fde8983e233a2bede1c7fe92269
Ben <39120147+bennygdev@users.noreply.github.com>
2023-01-17 11:08:30 +0800

    gave error message valid arguments not given
    Updated the code to where when the user does not give proper arguments or incorerct modes, the program will stop and output a error message to the user. this is implemented so that the program and work properly than previously.

--- namegenerator/index.js
+++ namegenerator/index.js
@@ -6,38 +6,43 @@ const words = process.argv[3];
 const quantity = process.argv[4];
 const wantNumber = process.argv[5];
 
-if(mode == 'dashed') {
-    for (let i = 0; i < quantity; i++) {
-        let name;
-        if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-            name = generate({words: words, number: true}).dashed;
-        } else {
-            name = generate({words: words}).dashed;
-        }
-        console.log(colors.green(name));
-    }
-}
-else if (mode == 'spaced') {
-    for (let i = 0; i < quantity; i++) {
-        let name;
-        if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-            name = generate({words: words, number: true}).spaced;
-        } else {
-            name = generate({words: words}).spaced;
-        }
-        console.log(colors.green(name));
-    }
-}
-else if (mode == 'raw') {
-    for (let i = 0; i < quantity; i++) {
-        let name;
-        if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
-            name = generate({words: words, number: true}).raw;
-        } else {
-            name = generate({words: words}).raw;
-        }
-        console.log(colors.green(name));
-    }
-} else {
+if(mode == undefined || words == undefined || quantity == undefined || wantNumber == undefined) {
     console.log(colors.red('Please enter a valid input'));
+} else {
+    if(mode == 'dashed') {
+        for (let i = 0; i < quantity; i++) {
+            let name;
+            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
+                name = generate({words: words, number: true}).dashed;
+            } else {
+                name = generate({words: words}).dashed;
+            }
+            console.log(colors.green(name));
+        }
+    }
+    else if (mode == 'spaced') {
+        for (let i = 0; i < quantity; i++) {
+            let name;
+            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
+                name = generate({words: words, number: true}).spaced;
+            } else {
+                name = generate({words: words}).spaced;
+            }
+            console.log(colors.green(name));
+        }
+    }
+    else if (mode == 'raw') {
+        for (let i = 0; i < quantity; i++) {
+            let name;
+            if (wantNumber === "yes" || wantNumber === "y" || wantNumber === "Y" || wantNumber === "YES") {
+                name = generate({words: words, number: true}).raw;
+            } else {
+                name = generate({words: words}).raw;
+            }
+            console.log(colors.green(name));
+        }
+    } 
+    else {
+        console.log(colors.red('Invalid input, please try again'));
+    }
 }
